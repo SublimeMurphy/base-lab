2ee9f8524aab67dd7dd884660dcfd4df44c3e590
Yunus Emre ARAS <yemrearas@Yunus-MacBook-Air.local>
2025-10-28 00:05:58 +0300

    fixes

--- app/farcaster/manifest.json/route.ts
+++ app/farcaster/manifest.json/route.ts
@@ -6,6 +6,13 @@ export const revalidate = 3600;
 export function GET(request: NextRequest) {
   const origin = request.nextUrl.origin;
   const manifest = withValidManifest({
+    accountAssociation: {
+      header:
+        "eyJmaWQiOjE0MDE0NzUsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHg0NjAwMTgxMDMyOUVlZGVkMWEzQ0ZhNzI3RTdEQjRhMUEyYUJFODk3In0",
+      payload: "eyJkb21haW4iOiJtaW5pLW1hdGgtcXVpei52ZXJjZWwuYXBwIn0",
+      signature:
+        "VLHh4YafS0yaBW1mJBJH3R4hnaMPRzZyp5cqXIoh6tFrhzvA5tbUDWu3cr1REXJwecG7V++7RX57uaV0UF2Udhw=",
+    },
     miniapp: {
       version: "1",
       name: "Mini Math Quiz",
